ecbd78233cbb3d3a6738a8aeeac44527ee700a7b
Kleverson Kenji <kleversonkenji@gmail.com>
2022-04-18 22:57:34 -0300

    responsive sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
 
 import { Logo } from "./Logo";
 import { SearchBox } from "./SearchBox";
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+import { RiMenuLine } from "react-icons/ri";
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   // base mantém visível, large muda para invisível
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -23,6 +27,16 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        ></IconButton>
+      )}
       <Logo />
 
       {isWideVersion && <SearchBox />}
