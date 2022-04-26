46e164a9e9e4570db76882049ce72d984634bc69
Kleverson Kenji <kleversonkenji@gmail.com>
2022-04-26 12:55:29 -0300

    pagination component and logic implementation

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -113,7 +113,11 @@ export default function UsersList() {
                   })}
                 </Tbody>
               </Table>
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={200}
+                currentPage={5}
+                onPageChange={() => {}}
+              />
             </>
           )}
         </Box>
