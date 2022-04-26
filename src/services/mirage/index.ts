f3ccf6d89b3349488dea3aeaee31be72a0fc719f
Kleverson Kenji <kleversonkenji@gmail.com>
2022-04-26 12:11:36 -0300

    implementing pagination logic

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from "miragejs";
+import { createServer, Factory, Model, Response } from "miragejs";
 import faker from "faker";
 
 type User = {
@@ -31,14 +31,31 @@ export function makeServer() {
     // user = nome do factory
     // 200 = quantos objetos criar
     seeds(server) {
-      server.createList("user", 10);
+      server.createList("user", 200);
     },
 
     routes() {
       this.namespace = "api";
       this.timing = 750;
 
-      this.get("/users");
+      // page = qual página mostrar
+      // per_page = quantos registros mostrar por página
+      this.get("/users", function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all("user").length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        );
+
+        return new Response(200, { "x-total-count": String(total) }, { users });
+      });
+
       this.post("/users");
 
       this.namespace = "";
